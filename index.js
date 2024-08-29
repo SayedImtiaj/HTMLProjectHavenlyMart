2118111059c6579d7f184e799d170d7c981e6e70
Imtiajtasif <imtiajalamtawsif@gmail.com>
2024-08-29 12:30:01 +0600

    Add project files.

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+var port = 2024;
+app.listen(port);
+console.log("Listening on port: " + port);
